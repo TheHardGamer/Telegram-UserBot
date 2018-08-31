8002b67fff2ecf19a49979df9da4ba25dfb6e4e6
The Hard Gamer V@RuN <gamelovr695@gmail.com>
2018-08-31 19:19:04 +0530

    Fix NSFW stuff

--- app.py
+++ app.py
@@ -813,15 +813,17 @@ async def ud(e):
 @bot.on(events.NewMessage(outgoing=True, pattern='.boobs'))
 @bot.on(events.MessageEdited(outgoing=True, pattern='.boobs'))
 async def boobs(e):
-    nsfw = requests.get('http://api.obutts.ru/noise/1').json()[0]["id"]
-    urllib.request.urlretrieve("http://media.obutts.ru/noise_preview/{}.jpg".format(nsfw), "{}.jpg".format(nsfw))
-    await bot.send_file(e.chat_id, "{}.jpg".format(nsfw))
+    nsfw = requests.get('http://api.oboobs.ru/noise/1').json()[0]["preview"]
+    urllib.request.urlretrieve("http://media.oboobs.ru/{}".format(nsfw), "*.jpg")
+    os.rename('*.jpg', 'k.jpg')
+    await bot.send_file(e.chat_id, "k.jpg")
 @bot.on(events.NewMessage(outgoing=True, pattern='.butts'))
 @bot.on(events.MessageEdited(outgoing=True, pattern='.butts'))
 async def butts(e):
-    nsfw = requests.get('http://api.oboobs.ru/noise/1').json()[0]["id"]
-    urllib.request.urlretrieve("http://media.oboobs.ru/noise_preview/{}.jpg".format(nsfw), "{}.jpg".format(nsfw))
-    await bot.send_file(e.chat_id, "{}.jpg".format(nsfw))
+    nsfw = requests.get('http://api.obutts.ru/noise/1').json()[0]["preview"]
+    urllib.request.urlretrieve("http://media.obutts.ru/{}".format(nsfw), "*.jpg")
+    os.rename('*.jpg', 'k.jpg')
+    await bot.send_file(e.chat_id, "k.jpg")
 @bot.on(events.NewMessage(outgoing=True, pattern='.tts'))
 @bot.on(events.MessageEdited(outgoing=True, pattern='.tts'))
 async def tts(e):
